1182bea275872eeac6d799d6b10554452bb60480
cjh980402 <9804cjh@naver.com>
2021-09-26 13:17:14 +0900

    Add options to download function

--- src/functions/download.ts
+++ src/functions/download.ts
@@ -1,4 +1,4 @@
-import { YoutubeVideoFormat } from "../structures/YoutubeVideo";
+import { DownloadOptions, YoutubeVideoFormat } from "../structures/YoutubeVideo";
 import { downloadFromVideo } from "./downloadFromVideo";
 import { getVideoInfo } from "./getVideoInfo";
 
@@ -7,21 +7,21 @@ import { getVideoInfo } from "./getVideoInfo";
  * @param urlOrId The url or id of the song to download its stream.
  * @param format The format to use for the song.
  */
-export async function download(urlOrId: string, format?: YoutubeVideoFormat) {
+export async function download(urlOrId: string, format?: YoutubeVideoFormat, options?: DownloadOptions) {
     const video = await getVideoInfo(urlOrId)
 
     if (format) {
         // If a format was provided, we can just use it instead.
-        return downloadFromVideo(video, format)
+        return downloadFromVideo(video, format, options)
     } else {
         // This format is suitable for music bots.
         const opus = video.formats.find(c => c.codec === "opus" && c.hasAudio && c.url && !c.hasVideo)
 
         if (opus) {
-            return downloadFromVideo(video, opus)
+            return downloadFromVideo(video, opus, options)
         } else {
             // This will last available format.
-            return downloadFromVideo(video, video.formats[video.formats.length - 1])
+            return downloadFromVideo(video, video.formats[video.formats.length - 1], options)
         }
     }
 }
