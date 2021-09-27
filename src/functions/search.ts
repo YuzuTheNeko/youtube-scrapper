df96ae9b66acbef1bf36cf458688001db0d6cd46
cjh980402 <9804cjh@naver.com>
2021-09-27 19:27:14 +0900

    Add type option for search function

--- src/functions/search.ts
+++ src/functions/search.ts
@@ -5,12 +5,22 @@ import { ErrorCodes } from "../util/constants";
 import { noop } from "../util/noop";
 import { Util } from "../util/Util";
 
-export async function search(query: string) {
+enum SearchType {
+    "video" = "EgIQAQ%3D%3D",
+    "playlist" = "EgIQAw%3D%3D",
+    "channel" = "EgIQAg%3D%3D"
+}
+
+export async function search(query: string, type?: string) {
     const params = new URLSearchParams()
-    
+
     params.append("search_query", query)
 
     params.append("hl", "en")
+
+    if (type && SearchType[type]) {
+        params.append("sp", SearchType[type])
+    }
 
     const request = await axios.get<string>(`${Util.getYTSearchURL()}?${params}`).catch(noop)
 
